b12b34ee15e7406fcabca76403ab44c874075acc
Gabriel Menino <grmenino@gmail.com>
2025-03-14 22:34:08 -0300

    Refactor

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -11,57 +11,71 @@ import java.util.Scanner;
 
 public class Game {
 
-    public static void main(String[] args) {
+    //---Processors---
+    SceneProcessor sceneProcessor = new SceneProcessor();
+    Map<Integer, Scene> scenes = sceneProcessor.processScenes();
 
-        //---Processors---
-        SceneProcessor sceneProcessor = new SceneProcessor();
-        Map<Integer, Scene> scenes = sceneProcessor.processScenes();
+    //Input scanner
+    Scanner scanner = new Scanner(System.in);
 
-        //Input scanner
-        Scanner scanner = new Scanner(System.in);
+    //Player
+    Player player;
 
+    public Game() {
         //Scene init
         Scene actualScene = scenes.get(0);
         //Player init
         Player player = new Player("Menino", 100, 20, JobEnum.WARRIOR, 100);
 
         while(actualScene != null) {
-            System.out.println("| Name: " + player.getName() +
-                    "  --  HP: " + player.getHp() +
-                    "  --  MP: " + player.getMp() +
-                    "  --  Class: " + player.getJob().getJobName() +
-                    "  --  Gold: " + player.getGold() +
-                    " |\n");
+            //Player stats (at the top of every scene)
+            printPlayerStats();
 
             System.out.println(actualScene.getDescription() + "\n");
 
             List<Option> options = actualScene.getOptions();
 
-            int optCount = 1;
-            for(Option option : options) {
-                System.out.println(optCount + " - " + option.getDescription());
-                optCount++;
-            }
+            //Show scene options
+            printOptions(options);
 
+            //Get player choice
             short input = scanner.nextShort();
 
+            //Validate option
             if(options.size() < input) {
                 System.out.println("Invalid option!");
                 continue;
             }
 
+            //Process selected scene
             Option selectedOption = options.get(input - 1);
-            Scene selectedScene = scenes.get(selectedOption.getDestinationScene());
 
             if(selectedOption.getAction() != null) {
                 ActionProcessor.processAction(player, selectedOption.getAction());
             }
 
-            actualScene = selectedScene;
+            actualScene = scenes.get(selectedOption.getDestinationScene());
         }
 
         System.out.println("Finish");
 
+    }
+
+    private void printPlayerStats() {
+        System.out.println("| Name: " + player.getName() +
+                "  --  HP: " + player.getHp() +
+                "  --  MP: " + player.getMp() +
+                "  --  Class: " + player.getJob().getJobName() +
+                "  --  Gold: " + player.getGold() +
+                " |\n");
+    }
+
+    private void printOptions(List<Option> options) {
+        int optCount = 1;
+        for(Option option : options) {
+            System.out.println(optCount + " - " + option.getDescription());
+            optCount++;
+        }
     }
 
 }
