7f83fc0773640f60a4826f5e7d1c66933fc87ae9
Gabriel Menino <grmenino@gmail.com>
2025-03-15 22:34:33 -0300

    Save and overwrite game

---
+++ src/main/java/model/SaveState.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.time.LocalDate;
+
+public class SaveState {
+
+    LocalDate date;
+    Player player;
+    Scene actualScene;
+
+}
--- src/main/java/processor/SaveLoadProcessor.java
+++ src/main/java/processor/SaveLoadProcessor.java
@@ -1,25 +1,40 @@
 package processor;
 
+import com.google.gson.Gson;
+import lombok.Setter;
+import model.SaveState;
+
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class SaveLoadProcessor {
 
     private final String SAVEFILE_PATH = "savefiles";
 
+    @Setter
+    private SaveState saveState;
+
     //Create savefile directory
     public SaveLoadProcessor(){
         new File(SAVEFILE_PATH).mkdirs();
     }
 
-    //Save actual state
+    //Save game state
     public void save(String filename) {
         try {
             File saveFile = new File(buildSavePath(filename));
-            saveFile.createNewFile();
+
+            if (!saveFile.exists()) {
+                saveFile.createNewFile();
+            }
+
+            FileWriter writer = new FileWriter(saveFile, false);
+            writer.write(new Gson().toJson(saveState));
+            writer.close();
 
             System.out.println("Saved successfully.");
-        } catch (IOException e) {
+        } catch (IOException exception) {
             System.out.println("Save failed.");
         }
     }
@@ -33,12 +48,9 @@ public class SaveLoadProcessor {
     private void listFiles() {
         File directory = new File(SAVEFILE_PATH);
 
-        // Verificar se o caminho é uma pasta
         if (directory.exists() && directory.isDirectory()) {
-            // Listar os arquivos da pasta
             String[] files = directory.list();
 
-            // Se houver arquivos, imprimir cada um deles
             if (files != null) {
                 for (String file : files) {
                     System.out.println(file);
